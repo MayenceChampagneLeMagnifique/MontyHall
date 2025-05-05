687f462e2edd277e7827c29120044da8a8df829c
William-Dionne-B <2349786@cegeplimoilou.ca>
2025-05-05 13:16:36 -0400

    Écriture de JouerParties dans Jeu.java

--- MontyHall/src/Jeu.java
+++ MontyHall/src/Jeu.java
@@ -10,9 +10,10 @@ import java.util.Scanner;
  * Cette classe permet de créer le jeu
  */
 public class Jeu {
+
     private List<Partie> listeParties = new ArrayList();
 
-    public void LancerPartie(){
+    public void lancerPartie() {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Bienvenue au problème de Monty Hall");
@@ -27,22 +28,22 @@ public class Jeu {
         String nom = s.nextLine();
         Joueur joueur = new Joueur(nom);
 
-        if (reponse.equalsIgnoreCase("M")){
+        if (reponse.equalsIgnoreCase("M")) {
             JouerPartieManuel(joueur);
         }
 
-        if (reponse.equalsIgnoreCase("A")){
+        if (reponse.equalsIgnoreCase("A")) {
             System.out.println("Combien de partie voulez-vous jouer ?");
             int nombrePartie = s.nextInt();
             JouerParties(joueur, nombrePartie);
         }
     }
 
-    public void ExporterParties(String path){
+    public void ExporterParties(String path) {
         try {
             PrintWriter fichier = new PrintWriter(new FileWriter(path));
-            for (Partie partie : listeParties){
-                fichier.println(partie);
+            for (Partie partie : listeParties) {
+                fichier.println(partie.toString());
                 fichier.println("\n");
             }
 
@@ -53,23 +54,35 @@ public class Jeu {
         }
     }
 
-    public void LancerAnalyse(int nombreParties){
+//    public void LancerAnalyse(int nombreParties) {
+//
+//    }
 
-    }
-
-    public void JouerParties(Joueur joueur, int NombreParties){
+    public void JouerParties(Joueur joueur, int NombreParties) {
         Scanner s = new Scanner(System.in);
 
         System.out.println("Combien de fois sur 100 voulez vous changer de porte si la première est vide ?");
-        String pourcentage = s.nextLine();
+        int pourcentage = 150;
+        do {
+            pourcentage = s.nextInt();
+            if (pourcentage < 0 || pourcentage > 100) {
+                System.out.println("Vous n'avez pas entrée une valeur entre 0 et 100");
+            }
+        }
+        while (pourcentage < 0 || pourcentage > 100);
 
     }
 
-    public void JouerPartieManuel(Joueur joueur){
+    public void JouerPartieManuel(Joueur joueur) {
         Scanner s = new Scanner(System.in);
 
         System.out.println();
         String s1 = s.nextLine();
+
+    }
+    public static void main(String[] args) {
+        Jeu jeu = new Jeu();
+        jeu.lancerPartie();
 
     }
 }
