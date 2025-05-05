eb98791bbf77d7c04cd1001f48902658e45a6b79
MayenceChampagne <2441356@cegeplimoilou.ca>
2025-05-05 12:49:46 -0400

    Écriture de la classe Porte.java, sans la JavaDoc

--- MontyHall/src/Porte.java
+++ MontyHall/src/Porte.java
@@ -3,4 +3,17 @@
  */
 
 public class Porte {
+    private Prix prix;
+
+    public Porte(Prix prix) {
+        setPrix(prix);
+    }
+
+    public Prix getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Prix prix) {
+        this.prix = prix;
+    }
 }
