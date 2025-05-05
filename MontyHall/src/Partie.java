49608421a21ece1eb0e1e5d1d7e5812e11124b52
MayenceChampagne <2441356@cegeplimoilou.ca>
2025-05-05 13:17:41 -0400

    Écriture de la classe creerPortes() ainsi que le getter et définition des variables et constantes dans Partie.java

--- MontyHall/src/Partie.java
+++ MontyHall/src/Partie.java
@@ -1,8 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Cette classe permet d'initialiser une partie
- *
  */
 
 public class Partie {
+    private final int NOMBRE_PORTES = 3;
+    private List<Porte> listePortes = new ArrayList<>();
+    private static final Prix PRIX_DEFAUT = Prix.AUCUN_PRIX;
 
+    public Partie() {
+        creerPortes();
+    }
+
+    private void creerPortes() {
+        Random r = new Random();
+
+        for (int i = 0; i < NOMBRE_PORTES; i++) {
+            listePortes.add(new Porte(PRIX_DEFAUT));
+        }
+
+        listePortes.get(r.nextInt(NOMBRE_PORTES)).setPrix(Prix.VOITURE);
+    }
+
+    public List<Porte> getListePortes() {
+        return listePortes;
+    }
+
+    public int getNOMBRE_PORTES() {
+        return NOMBRE_PORTES;
+    }
 }
