d6fd5206a700ca318814af50cd326696ea8ca6fe
Mayence Champagne <2441356@cegeplimoilou.ca>
2025-05-18 18:24:36 -0400

    Ajout de la classe de tests UtilTest.java, qui permet de tester l'interface Util.java

---
+++ MontyHall/src/Tests/UtilTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+class UtilTest {
+
+    /**
+     * La méthode poserQuestionString() utilise un scanner, elle n'est donc pas éligible à être testée.
+     * Cette méthode devrait normalement tester la méthode poserQuestionString(), qui pose une question dont la réponse est une chaîne de caractères.
+     */
+    @Test
+    void poserQuestionString() {
+    }
+
+    /**
+     * La méthode poserQuestionInt() utilise un scanner, elle n'est donc pas éligible à être testée.
+     * Cette méthode devrait normalement tester la méthode poserQuestionInt(), qui pose une question dont la réponse est un entier.
+     */
+    @Test
+    void poserQuestionInt() {
+    }
+}
